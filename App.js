cfb9ba94a038bcf6e20df8fa64a9f06544c3f223
AtumaDavid <atumadavid35@gmail.com>
2023-09-21 13:06:42 +0100

    Navigation and tab

--- App.js
+++ App.js
@@ -1,22 +1,66 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import CurrentWeather from "./src/screens/CurrentWeather";
-import UpcomingWeather from "./src/screens/UpcomingWeather";
-import City from "./src/screens/City";
+// import { StatusBar } from "expo-status-bar";
+// import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+// import { Feather } from "@expo/vector-icons";
+// import CurrentWeather from "./src/screens/CurrentWeather";
+// import UpcomingWeather from "./src/screens/UpcomingWeather";
+// import City from "./src/screens/City";
+// React Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./src/components/Tabs";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-      {/* <UpcomingWeather /> */}
-      {/* <City /> */}
-    </View>
+    <NavigationContainer>
+      <Tabs />
+      {/* <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "grey",
+        }}
+      >
+        <Tab.Screen
+          name={"Current"}
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={"droplet"}
+                size={25}
+                color={focused ? "blue" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={"Upcoming"}
+          component={UpcomingWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={"clock"}
+                size={25}
+                color={focused ? "blue" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={"City"}
+          component={City}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={"home"}
+                size={25}
+                color={focused ? "blue" : "black"}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator> */}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # API from: openweatherapp. (https://openweathermap.org/forecast5)
 
-# icons from `https://icons.expo.fyi/Index`
+# icons from (`https://icons.expo.fyi/Index`)
+
+# Navigation (**React Navigation (Builtin In Expo)**) (https://reactnavigation.org/)
+
+- npm install @react-navigation/native
+- npx expo install react-native-screens react-native-safe-area-context
+
+**For Bottom Tabs Navigation**
+
+- npm install @react-navigation/bottom-tabs
